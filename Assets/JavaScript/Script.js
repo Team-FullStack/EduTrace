fdde80b12b9ee4482f0a847d85cf8f3f09dc6696
Kaustubh patil <pkaustubh401@gmail.com>
2022-03-11 14:47:14 +0530

    Fixed navbar , Added first section

--- Assets/JavaScript/Script.js
+++ Assets/JavaScript/Script.js
@@ -1,12 +1,10 @@
 const menu_toggle = document.querySelector(".menu-toggle");
 const site_nav = document.querySelector(".site-nav");
-
+const nav_container = document.querySelector(".nav-container");
 // hamburger toggle
 menu_toggle.addEventListener("click", function () {
-  console.log(site_nav);
-  console.log(menu_toggle);
-  console.log(this);
   site_nav.classList.toggle("site-nav--open");
+  nav_container.classList.toggle("nav-container--open");
   //   $(this).toggleClass("open");
   this.classList.toggle("open");
 });
