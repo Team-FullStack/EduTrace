244387daf765b409b9ccb454bd9deb7ce26e988a
Kaustubh patil <pkaustubh401@gmail.com>
2022-03-13 17:40:53 +0530

    Added smooth scroll and clickable links

--- Assets/JavaScript/Script.js
+++ Assets/JavaScript/Script.js
@@ -1,10 +1,22 @@
 const menu_toggle = document.querySelector(".menu-toggle");
 const site_nav = document.querySelector(".site-nav");
 const nav_container = document.querySelector(".nav-container");
+
+const nav_links = document.querySelectorAll(".site-nav a");
+
 // hamburger toggle
 menu_toggle.addEventListener("click", function () {
   site_nav.classList.toggle("site-nav--open");
   nav_container.classList.toggle("nav-container--open");
   //   $(this).toggleClass("open");
   this.classList.toggle("open");
+});
+
+nav_links.forEach((nav_link) => {
+  nav_link.addEventListener("click", function () {
+    site_nav.classList.remove("site-nav--open");
+    nav_container.classList.remove("nav-container--open");
+    //   $(this).toggleClass("open");
+    this.classList.remove("open");
+  });
 });
